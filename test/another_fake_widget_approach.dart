719016f770abf2fe432b2f75b872d74f91764bc7
MoSala95 <hangor95@gmail.com>
2021-06-13 23:33:55 +0200

    fakeWeather_widget_test

--- test/another_fake_widget_approach.dart
+++ test/another_fake_widget_approach.dart
@@ -1,30 +1,49 @@
 
 import 'package:bloc_test/bloc_test.dart';
 import 'package:dartz/dartz.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mockito/mockito.dart';
 import 'package:weather_testing_app/data/weather_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:weather_testing_app/bloc/weather_bloc.dart';
 
 import 'package:weather_testing_app/models/weather.dart';
+import 'package:weather_testing_app/pages/weather_search_page.dart';
 
 class MockWeatherRepository extends Mock implements WeatherRepository {}
 
 
 void main() {
   MockWeatherRepository mockWeatherRepository;
+  WeatherBloc weatherBloc;
 
   setUp(() {
     mockWeatherRepository = MockWeatherRepository();
+    weatherBloc = WeatherBloc(mockWeatherRepository);
   });
-  blocTest(
-    'emits [WeatherLoading, WeatherLoaded] when successful',
-    build: () {
-      when(mockWeatherRepository.fetchWeather.call(any))
-          .thenAnswer((_) async => right(Weather(main: Main(temp: 22.5))));
-      return WeatherBloc(mockWeatherRepository);
-    },
-    act: (bloc) => bloc.add(GetWeather('cairo')),
-    expect:()=> [isA<WeatherLoading>(),isA<WeatherLoaded>()],
-  );
+
+  group('GetWeather', () {
+    testWidgets('Weather success test', (WidgetTester tester) async {
+      await tester.runAsync(() async {
+        await tester.pumpWidget(MaterialApp(home:  BlocProvider.value(
+          value: weatherBloc,
+          child: WeatherSearchPage(),
+
+        ),));
+        expect(find.byType(CityInputField), findsWidgets);
+        when(mockWeatherRepository.fetchWeather.call(any))
+            .thenAnswer((_) async => right(Weather(main: Main(temp: 22.5))));
+         weatherBloc.add(GetWeather("cairo"));
+        await tester.pumpAndSettle();
+        expectLater(
+          weatherBloc.stream,
+          emitsInOrder([
+            isA<WeatherLoading>(),
+            isA<WeatherLoaded>(),
+          ]),
+        );
+      });
+    });
+  });
 }
